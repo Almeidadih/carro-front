ff7e12746d6cd99f776d234870dad5bd3077bb29
diego <almeidadieo488@gmail.com>
2025-11-19 21:27:15 -0300

    Commit inicial - estrutura do projeto carro_front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Carroslist } from './components/carroslist/carroslist';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "carros", component: Carroslist}
+];
